f2e53b0eb0a817e0eaf03b5d4f67c5272e1dde4e
frkfrisor-source <frkfrisor@gmail.com>
2026-02-18 12:50:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dk.tina.madplanmester',
+  appName: 'Madplan',
+  webDir: 'dist/public', 
+};
+
+export default config;
